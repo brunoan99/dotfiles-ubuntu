92cd81e68f02e0ed548ea489921f2b807fb88c3f
Bruno Andrade <brunoan99@gmail.com>
2025-03-30 05:40:34 -0300

    feat: add system specific code to setup_tools and install script

---
+++ system/setup_tools.sh
@@ -0,0 +1,22 @@
+. $DOT/utils/print_functions.sh
+
+setup_tools() {
+  info "Installing packages"
+  declare -a packages=("curl" "wget" "git" "zsh"
+  "autoconf" "automake" "binutils" "bison"
+  "debugedit" "fakeroot" "file" "findutils" "flex" "gawk"
+  "gcc" "gettext" "grep" "groff" "gzip" "libtool" "m4" "make"
+  "patch" "pkgconf" "sed" "sudo" "texinfo" "which"
+  "imagemagick" "ffmpeg" "less" "fzf" "net-tools" "zoxide")
+  for package in "${packages[@]}"; do
+    info "Installing $package"
+    # Check if the package is already installed on Ubuntu system
+    if ! dpkg -s "$package" &> /dev/null; then
+      # Install the package using apt
+      sudo apt-get install -y "$package"
+    else
+      info "$package is already installed."
+    fi
+  done
+
+}
