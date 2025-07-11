993f5a5ab233bd4ecb1f0f6671a7f4bb4b1d0e7e
Bruno Andrade <brunoan99@gmail.com>
2025-07-11 00:46:10 -0300

    feat: add check if apt upgrade was already runned in same day before ask for upgrade

--- zsh/startup.zsh
+++ zsh/startup.zsh
@@ -2,10 +2,15 @@ ask_for_upgrade() {
   echo -n "Do you want to run system upgrade? [y/N]: "
   read answer
   case "$answer" in
-    [Yy]* ) update
-    ;;
-    * ) echo "Skipping upgrade."
-    ;;
+    [Yy]* ) update ;;
+    * ) echo "Skipping upgrade." ;;
   esac
+}
+
+verify_for_upgrade() {
+  local last_upgrade_date=$(ls -l --time-style=long-iso /var/cache/apt/pkgcache.bin | cut -d ' ' -f4)
+  local current_date=$(date +%Y-%m-%d)
+
+  if [[ "$last_upgrade_date" != "$current_date" ]]; then ask_for_upgrade; fi
 }
 
