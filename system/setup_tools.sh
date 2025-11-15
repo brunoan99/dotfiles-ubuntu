395872e31b9554e7f756688de58d59c0c2fc6b1c
Bruno Andrade <brunoan99@gmail.com>
2025-11-15 09:48:54 +0100

    fix: package install script

--- system/setup_tools.sh
+++ system/setup_tools.sh
@@ -1,22 +1,27 @@
+#!/usr/bin/env bash
+
 . $DOT/utils/print_functions.sh
 
 setup_tools() {
   info "Installing packages"
-  declare -a packages=("curl" "wget" "git" "zsh"
-  "autoconf" "automake" "binutils" "bison"
-  "debugedit" "fakeroot" "file" "findutils" "flex" "gawk"
-  "gcc" "gettext" "grep" "groff" "gzip" "libtool" "m4" "make"
-  "patch" "pkgconf" "sed" "sudo" "texinfo" "which"
-  "imagemagick" "ffmpeg" "less" "fzf" "net-tools" "zoxide" "neovim")
+  declare -a packages=("curl" "wget" "git" "zsh" "autoconf" "automake"
+   "binutils" "bison" "debugedit" "fakeroot" "file" "findutils" "flex" "gawk"
+   "gcc" "gettext" "grep" "groff" "gzip" "libtool" "m4" "make" "patch" "pkgconf"
+   "sed" "sudo" "texinfo" "gnu-which" "imagemagick" "ffmpeg" "less" "fzf"
+   "net-tools" "zoxide" "neovim"
+  )
   for package in "${packages[@]}"; do
-    info "Installing $package"
     # Check if the package is already installed on Ubuntu system
     if ! dpkg -s "$package" &> /dev/null; then
       # Install the package using apt
       sudo apt-get install -y "$package"
+      success "Installed $package"
     else
       info "$package is already installed."
     fi
   done
-
+  echo ""
+  success "Packages Installed"
 }
+
+setup_tools
