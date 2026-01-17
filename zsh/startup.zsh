9c05f82dbd35ce6c9f5560a54ffa74a7206cabc4
Bruno Andrade <brunoan99@gmail.com>
2026-01-17 13:27:29 -0300

    feat: add check to only perfom ask for upgrade if in WSL session

--- zsh/startup.zsh
+++ zsh/startup.zsh
@@ -1,5 +1,13 @@
 #!/bin/zsh
 
+check_wsl() {
+  if [ -n "$WT_SESSION" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
 ask_for_upgrade() {
   echo -n "Do you want to run system update? [y/N]: "
   read -k1 answer
@@ -11,8 +19,9 @@ ask_for_upgrade() {
 }
 
 verify_for_upgrade() {
-  local last_upgrade_date=$(ls -l --time-style=long-iso /var/cache/apt/pkgcache.bin | cut -d ' ' -f4)
-  local current_date=$(date +%Y-%m-%d)
-
-  if [[ "$last_upgrade_date" != "$current_date" ]]; then ask_for_upgrade; fi
+  if check_wsl; then
+    local last_upgrade_date=$(ls -l --time-style=long-iso /var/cache/apt/pkgcache.bin | cut -d ' ' -f4)
+    local current_date=$(date +%Y-%m-%d)
+    if [[ "$last_upgrade_date" != "$current_date" ]]; then ask_for_upgrade; fi
+  fi
 }
